2c46f6257d995a29be831300cdc094dfe02e183b
Jo-Anne Snip <tali-surimeid@hotmail.com>
2024-12-10 14:32:47 +0100

    created step 1 in Eventspage

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import React, { useState, useEffect } from "react";
+import { Heading, Box, Text } from "@chakra-ui/react";
 
 export const EventsPage = () => {
-  return <Heading>List of events</Heading>;
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const response = await fetch("http://localhost:3000/events");
+      const data = await response.json();
+      setEvents(data);
+    };
+
+    fetchEvents();
+  }, []);
+
+  return (
+    <Box>
+      <Heading>Event List</Heading>;
+      {events.map((event) => (
+        <Box key={event.id}>
+          <Heading>{event.title}</Heading>
+          <Text>{event.description}</Text>
+        </Box>
+      ))}
+    </Box>
+  );
 };
