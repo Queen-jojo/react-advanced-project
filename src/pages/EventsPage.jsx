bf8a770e238921430601e951aefb0c346dd2541c
Jo-Anne Snip <tali-surimeid@hotmail.com>
2024-12-12 11:46:39 +0100

    finishing up EventsPage

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { Heading, Box, Text, Image, Link } from "@chakra-ui/react";
+import {
+  Heading,
+  Box,
+  Text,
+  Image,
+  Link,
+  Input,
+  Select,
+} from "@chakra-ui/react";
 import EventForm from "./EventForm";
 
 export const EventsPage = () => {
   const [events, setEvents] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedCategories, setSelectedCategories] = useState([]);
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -15,11 +25,47 @@ export const EventsPage = () => {
     fetchEvents();
   }, []);
 
+  const uniqueCategories = [
+    ...new Set(events.flatMap((event) => event.categories)),
+  ];
+
+  const filteredEvents = events.filter((event) => {
+    const lowerCaseTitle = event.title.toLowerCase();
+    const lowerCaseDescription = event.description.toLowerCase();
+    const lowerCaseSearchQuery = searchQuery.toLowerCase();
+
+    const matchesSearch =
+      lowerCaseTitle.includes(lowerCaseSearchQuery) ||
+      lowerCaseDescription.includes(lowerCaseSearchQuery);
+
+    const matchesCategories =
+      selectedCategories.lenght === 0 ||
+      event.categories.some((category) =>
+        selectedCategories.includes(category)
+      );
+
+    return matchesSearch && matchesCategories;
+  });
+
   return (
     <Box>
       <Heading>Event List</Heading>;
+      <Input
+        placeholder="Search Events"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <Select
+        multiplevalue={selectedCategories}
+        onChange={(selectedOptions) => setSelectedCategories(selectedOptions)}
+        options={uniqueCategories.map((category) => ({
+          value: category,
+          label: category,
+        }))}
+        placeholder="Filtered by Category"
+      />
       <EventForm />
-      {events.map((event) => (
+      {filteredEvents.map((event) => (
         <Box key={event.id}>
           <Link to={`/event/${event.id}`}>
             <Heading as="h3">{event.title}</Heading>
